d5390878c8c1095c6b43b81e1b32720811f6c526
Albert <alberto.suarez8@outlook.com>
2022-12-20 14:46:22 -0500

    Added inquirer questions and functionality

--- db/schema.sql
+++ db/schema.sql
@@ -6,7 +6,7 @@ USE company_db;
 CREATE TABLE department (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
     name VARCHAR(30) NOT NULL
-)
+);
 
 CREATE TABLE role (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -16,7 +16,7 @@ CREATE TABLE role (
     FOREIGN KEY (department_id)
     REFERENCES department(id)
     ON DELETE SET NULL
-)
+);
 
 CREATE TABLE employee (
     id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
@@ -30,4 +30,4 @@ CREATE TABLE employee (
     FOREIGN KEY (manager_id)
     REFERENCES employee(id)
     ON DELETE SET NULL
-)
+);
--- index.js
+++ index.js
@@ -2,3 +2,71 @@ const inquirer = require('inquirer');
 const mysql = require('mysql2');
 const cTable = require('console.table');
 
+const questions = [
+    {
+        type: 'list',
+        name: 'tracker',
+        message: 'What would you like to do?',
+        choices: [
+            'View All Departments',
+            'View All Roles',
+            'View All Employees',
+            'Add Department',
+            'Add Role',
+            'Add Employee',
+            'Update Employee Role',
+            'Quit',
+        ],
+        default: "View All Departments",
+        loop: true
+    }
+];
+
+function init() {
+    inquirer
+        .prompt(questions)
+        .then((answers) => {
+            switch (answers.tracker) {
+                case "View All Departments":
+                    
+                    init();
+                    return;
+                case "View All Roles":
+                    
+                    init();
+                    return;
+                case "View All Employees":
+                    
+                    init();
+                    return;
+                case "Add Department":
+                    
+                    init();
+                    return;
+                case "Add Role":
+
+                    init();
+                    return;
+                case "Add Employee":
+                    
+                    init();
+                    return;
+                case "Update Employee Role":
+                    
+                    init();
+                    return;
+                default:
+                    return;
+            }
+        })
+        .catch((error) => {
+            if (error.isTtyError) {
+                console.log("Could not be rendered in current environment")
+            } else {
+                console.log("Something went horribly wrong")
+                console.log(error)
+            }
+        });
+}
+
+init();
