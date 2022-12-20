4c17c041d4237ba0a99b2a4761a66d9a69606798
Albert <alberto.suarez8@outlook.com>
2022-12-20 13:55:36 -0500

    Refactored sql files

--- db/seeds.sql
+++ db/seeds.sql
@@ -4,22 +4,22 @@ VALUES ("Engineering"),
        ("Legal"),
        ("Sales");
 
-INSERT INTO role (title, salary)
-VALUES (Sales Lead, 100000),
-       (Salesperson, 80000),
-       (Lead Engineer, 150000),
-       (Software Engineer, 120000),
-       (Account Manager, 160000),
-       (Accountant, 125000),
-       (Legal Team Lead, 250000),
-       (Lawyer, 190000);
+INSERT INTO role (title, salary, department_id)
+VALUES ("Sales Lead", 100000, 4),
+       ("Salesperson", 80000, 4),
+       ("Lead Engineer", 150000, 1),
+       ("Software Engineer", 120000, 1),
+       ("Account Manager", 160000, 2),
+       ("Accountant", 125000, 2),
+       ("Legal Team Lead", 250000, 3),
+       ("Lawyer", 190000, 3);
 
-INSERT INTO employee (first_name, last_name)
-VALUES (Alberto, Suarez),
-       (John, Doe),
-       (Tim, Henson),
-       (Bradley, Adams),
-       (Bob, Smith),
-       (David, Henry),
-       (George, Wilson),
-       (Charles, Miller);
+INSERT INTO employee (first_name, last_name, role_id)
+VALUES ("Alberto", "Suarez", 1),
+       ("John", "Doe", 2),
+       ("Tim", "Henson", 3),
+       ("Bradley", "Adams", 4),
+       ("Bob", "Smith", 5),
+       ("David", "Henry", 6),
+       ("George", "Wilson", 7),
+       ("Charles", "Miller", 8);
